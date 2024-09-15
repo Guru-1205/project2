82a5154d1cad33f4a2e368da87f72f0c79b046fa
Guru-1205 <praveenkumartv@student.tce.edu>
2024-09-15 11:11:42 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+// routing -> Home ,about ,contact ,settings -> simple navigation bar
+import { BrowserRouter, Link, Routes,Route } from "react-router-dom";
+function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>Home page</div>
+  )
+}
+
+function About() {
+  return (
+    <div>About page</div>
+  )
+}
+
+function Contact() {
+  return (
+    <div>Contact page</div>
+  )
+}
+
+function App()
+{
+  return (
+    <BrowserRouter>
+    <nav>
+      <ul>
+        {/* localhost:3000/contact */}
+        <Link to="/"><li>Home</li></Link>
+        <Link to="/about"><li>About</li></Link>
+        <Link to="/contact"><li>Contact</li></Link>
+      </ul>
+      </nav>
+
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/contact" element={<Contact/>}/>
+      </Routes>
+      </BrowserRouter>
   );
 }
 
 export default App;
+// public folder -> index.html -> div tag with id="root" -> created a Reactdom with base as root 
+// root->App->Home
+// index.html -> div -> Home page content
+// root->App->About
+// index.html -> div -> ABout page content
+// localhost:3000/about
+// index.html -> div -> Contact page content
+// localhost:3000/contact
+// root->App->Contact
+// index.html -> based on the url -> we are rendering the component 
